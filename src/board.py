f1756fbdb14c15bc7620a5a5b1b20f9853a13531
aaanto <anto.juko.3080@student.uu.se>
2020-09-20 22:16:27 +0200

    Added board

--- src/board.py
+++ src/board.py
@@ -1,69 +1,97 @@
-class Piece:
-    Empty = 0
-    Black = 1
-    White = 2
+def printboard(board):
+  print("  "+board['0']+"-----------------------------"+board['1']+"-----------------------------"+board['2'])
+  print("  |⟍                            |                           ⟋ |")
+  print("  |  ⟍                          |                         ⟋   |")
+  print("  |    ⟍                        |                       ⟋     |")
+  print("  |      ⟍                      |                     ⟋       |")
+  print("  |        "+board['3']+"--------------------"+board['4']+"--------------------"+board['5']+"        |")
+  print("  |        | ⟍                  |                 ⟋  |        |")
+  print("  |        |   ⟍                |               ⟋    |        |")
+  print("  |        |     ⟍              |             ⟋      |        |")
+  print("  |        |       ⟍            |           ⟋        |        |")
+  print("  |        |         "+board['6']+"----------"+board['7']+"----------"+board['8']+"         |        |")
+  print("  |        |         |                     |         |        |")
+  print("  |        |         |                     |         |        |")
+  print("  |        |         |                     |         |        |")
+  print("  "+board['9']+"------------------"+board['10']+"                     "+board['11']+"---------"+board['12']+"--------"+board['13'])
+  print("  |        |         |                     |         |        |")
+  print("  |        |         |                     |         |        |")
+  print("  |        |         "+board['14']+"----------"+board['15']+"----------"+board['16']+"         |        |")
+  print("  |        |       ⟋            |            ⟍       |        |")
+  print("  |        |     ⟋              |              ⟍     |        |")
+  print("  |        |   ⟋                |                ⟍   |        |")
+  print("  |        | ⟋                  |                  ⟍ |        |")
+  print("  |        "+board['17']+"--------------------"+board['18']+"--------------------"+board['19']+"        |")
+  print("  |      ⟋                      |                      ⟍      |")
+  print("  |    ⟋                        |                        ⟍    |")
+  print("  |  ⟋                          |                          ⟍  |")
+  print("  |⟋                            |                            ⟍|")
+  print("  "+board['20']+"-----------------------------"+board['21']+"-----------------------------"+board['22']+"  ")
 
-class Board:
-    board = None
-
-    lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [9, 10, 11],
-        [12, 13, 14],
-        [15, 16, 17],
-        [18, 19, 20],
-        [21, 22, 23],
-        [0, 9, 21],
-        [3, 10, 18],
-        [6, 11, 15],
-        [8, 12, 17],
-        [5, 13, 20],
-        [2, 14, 23],
-        [0, 3, 6],
-        [2, 5, 8],
-        [15, 18, 21],
-        [17, 20, 23],
-        [1, 4, 7],
-        [16, 19, 22]
-    ]
 
 
-    def get_lines_for_position(self, position):
-        found_lines = []
 
-        for line in self.lines:
-            if (position in line):
-                found_lines.append(line)
+gameBoard = {'0': ' ', '1': ' ', '2': ' ',
+             '3': ' ', '4': ' ', '5': ' ',
+             '6': ' ', '7': ' ', '8': ' ',
+             '9': ' ',  '10': ' ', '11': ' ',
+             '12': ' ',  '13': ' ',  '14': ' ',
+             '15': ' ',  '16': ' ',  '17': ' ',
+             '18': ' ', '19': ' ', '20': ' ',
+             '21': ' ', '22': ' ',  '23': ' '}
 
-        return found_lines
 
-    def has_three_at_position(self, piece, position):
-        lines = self.get_lines_for_position(position)
-        for line in lines:
-            line_full = True
-            for position in line:
-                if (self.board[position] != piece):
-                    line_full = False
-                    break
-            if (line_full):
-                return True
+"""
 
-        return False
+this is the backup drawing
 
-    def get_other_piece(self, piece):
-        if (piece == Piece.Black):
-            return Piece.White
-        if (piece == Piece.White):
-            return Piece.Black
-        return Piece.Empty
+def printBoard(board):
+    print ('#######################################################');
+    print ('### ' + board['0'] + ' -------------------- '+ board['1'] + ' -------------------- ' + board['2'] + ' ###');
+    print('### | \ ################## | ################## / | ###')
+    print('### | # \ ################ | ################ / # | ###')
+    print ('### | ### ' + board['3'] + ' -------------- ' + board['4'] + ' -------------- ' + board['5'] + ' ### | ###');
+    print('### | ### | \ ############ | ############ / | ### | ###')
+    print('### | ### | # \ ########## | ########## / # | ### | ###')
+    print ('### | ### | ### ' + board['6'] + ' -------- ' + board['7'] + ' -------- ' + board['8'] + ' ### | ### | ###');
+    print('### | ### | ### | ################### | ### | ### | ###')
+    print('### | ### | ### | ################### | ### | ### | ###')
+    print('### ' + board['9'] + ' --- ' + board['10'] + ' --- ' + board['11'] + ' ################### ' + board['12'] + ' --- ' + board['13'] + ' --- ' + board['14'] + ' ###')
+    print('### | ### | ### | ################### | ### | ### | ###')
+    print('### | ### | ### | ################### | ### | ### | ###')
+    print ('### | ### | ### ' + board['15'] + ' -------- ' + board['16'] + ' -------- ' + board['17'] + ' ### | ### | ###');
+    print('### | ### | # / ########## | ########## \ # | ### | ###')
+    print('### | ### | / ############ | ############ \ | ### | ###')
+    print ('### | ### ' + board['18'] + ' -------------- ' + board['19'] + ' -------------- ' + board['20'] + ' ### | ###');
+    print('### | # / ################ | ################ \ # | ###')
+    print('### | / ################## | ################## \ | ###')
+    print ('### ' + board['21'] + ' -------------------- '+ board['22'] + ' -------------------- ' + board['23'] + ' ###');
+    print ('#######################################################');
 
-    def __getitem__(self, index):
-        return self.board[index]
+"""
 
-    def __setitem__(self, index, value):
-        self.board[index] = value
+def mainMenu():
+    print('### UU-GAME ###')
+    print('1. Play a new game')
+    print('2. How to play')
+    user_input = input('Please enter your choice from the menu and press enter: ')
 
-    def __init__(self):
-        self.board = [Piece.Empty] * 24
+    if user_input in ['1', '2'] :
+        if user_input == '1':
+            print("\033c")
+            printboard(gameBoard);
+        if user_input == '2':
+            print("\033c")
+            print("This is how you play....")
+            print("TBA")
+            menu_input = input('<- Back to main menu, input 1 and press enter: ')
+            if menu_input == '1':
+              print("\033c")
+              mainMenu();
+            else:
+                input('Please input 1 to get back to main menu: ')
+
+    else:
+        print('Invalid input, please enter your choice from the menu and press enter: ');
+        mainMenu()
+mainMenu();
